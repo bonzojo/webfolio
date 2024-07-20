f922a09acd03515304d135558012a03a1be9788a
John McEwan <bonzojo@proton.me>
2024-07-20 03:44:51 +0100

    background smoothie

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,19 @@ export default function Home() {
         <meta name="description" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+    
       <main>
-        <h1 className="font-jost">
-          Hi I am inconsolata. The quick brown fox jumped over the lazy dog!
-        </h1>
-        <h2 className="font-jetbrains">
-          A happy developer is a good developer!
-        </h2>
+        
+        <section className='h-screen overflow-hidden w-screen animated-background bg-gradient-to-r from-slate-800 via-sky-900 to-cyan-800'>
+          
+            <p className='font-jetbrains text-7xl text-white font-outline-4'>
+            Hello World! Why does Javascript want to make me cry? All I want is a pretty background.
+            </p>
+
+        </section>
       </main>
     </div>
+    
   );
 }
+
