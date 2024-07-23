37ed9d19471270169d91f3bbe50a86b258b8d47b
John McEwan <bonzojo@proton.me>
2024-07-23 04:29:43 +0100

    more placeholders and tailwind utility class shenanigans

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,7 @@ import { TfiClip } from "react-icons/tfi";
 import { TfiLinkedin } from "react-icons/tfi";
 import { TfiGithub } from "react-icons/tfi";
 
-{/*Import Images */}
-import placeholder from '../public/placeholder-portrait.png';
+{/*Import Icons */}
 import htmlIcon from '../public/icons/html96.svg';
 import cssIcon from '../public/icons/css96.svg';
 import jsIcon from '../public/icons/js96.svg';
@@ -16,6 +15,12 @@ import phpIcon from '../public/icons/php128.svg';
 import mysqlIcon from '../public/icons/mysql96.svg';
 import postgresIcon from '../public/icons/postgres96.svg';
 import pythonIcon from '../public/icons/python96.svg';
+
+{/* Import Images */}
+import placeholder from '../public/images/placeholder-portrait.png';
+import john from '../public/images/john.jpg';
+import indian from '../public/images/indian.jpg';
+import resume from '../public/images/resume.jpg';
 
 export default function Home() {
   return (
@@ -54,7 +59,7 @@ export default function Home() {
       <section className='relative m-0 p-0 h-screen w-full min-h-screen bg-gradient-to-r animated-background from-slate-800 via-sky-900 to-cyan-800'>
           
       {/*Portrait */}
-      <Image src={placeholder} className='absolute mt-28 p-4 left-4 sm:w-1/3 md:w-1/3 lg:w-1/3 xl:w-1/3'/>
+      <Image src={john} className='absolute mt-20 p-4 left-4 w-48 sm:w-1/3 md:w-1/3 lg:w-1/3 xl:w-1/3'/>
 
       {/* Typewriter Hero */}
         <div className='absolute mt-28 p-4 font-jetbrains text-white text-base sm:text-4xl sm:font-outline-4 md:text-5xl lg:text-7xl right-16'>
@@ -83,7 +88,8 @@ export default function Home() {
           />
           </div>
 
-          <p className='absolute font-jetbrains mt-64 p-24 py-16 -left-16 text-white text-base sm:py-24 sm:text-1xl md:py-36 md:text-2xl lg:py-60 lg:text-3xl xl:py-80 xl:text-4xl'>This is a little bit of blurb introducing myself in a summary that manages to cover a
+        {/* Short Bio */}
+        <p className='absolute font-jetbrains p-24 mt-48 py-16 -left-16 text-white text-base sm:py-24 sm:text-1xl md:py-36 md:mt-48 md:text-2xl lg:mt-56 lg:py-48 lg:text-3xl xl:py-48 xl:text-3xl xl:mt-72 xl:top-24'>This is a little bit of blurb introducing myself in a summary that manages to cover a
           few lines, making up a paragraph. Talk about the key points of your skills and your interests.
         </p>
       </section>
